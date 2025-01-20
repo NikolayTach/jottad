9d36e8148bfb8be9cc83f914a39873080feee551
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2025-01-20 13:04:56 -0800

    Update dodo1.c

--- dodo1.c
+++ dodo1.c
@@ -1,8 +1,7 @@
 #include<dodo1.h>
 int main(void)
 {
-set main to domain("@nogue.io")
-correspond to set('change domain scala-nogue.io to scala.nogue.io')
+set('change domain scala-nogue.io to scala.nogue.io')
 nprint('scala.nogue.io')
 return(0);
 }
