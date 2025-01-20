f93ea8aad8cbd6ab7278bc0c1db6cf354ea9ac80
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2025-01-20 12:12:47 -0800

    Add files via upload

---
+++ dodo1.c
@@ -0,0 +1,8 @@
+import "dodo1.h"
+fn:main();
+create domain set;
+ref 90-28-5235988593-2853
+set main to domain("@nogue.io")
+correspond to set('change domain scala-nogue.io to scala.nogue.io')
+nprint('scala.nogue.io')
+
