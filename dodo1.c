32fe64de5e2213fe8a760b216df774bf2998a0de
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2025-01-20 19:37:08 -0800

    Update dodo1.c

--- dodo1.c
+++ dodo1.c
@@ -1,7 +1,7 @@
 #include "dodo1.h"
 {
 asm("@nogue.io");
- system("bash -c \"scala-nogue.io to scala.nogue.io/main\"");
+system("bash -c \"scala-nogue.io to scala.nogue.io/main\"");
 nprint('scala.nogue.io');
 return(0);
 }
