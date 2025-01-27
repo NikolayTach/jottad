7c1ceb881aa3e99d984b5825da28cbf4828ae8ca
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2025-01-27 09:34:11 -0800

    Update root_spec.scala

--- root_spec.scala
+++ root_spec.scala
@@ -1,6 +1,7 @@
 val args: Array[String] = Array(NullFunction)
-java.io.FileNotFoundException;
-	class to ArraySringFunction java.base/java.io.FileInputStream.open0 NullFunction(Native Method)
+java.io.FileNotFoundException;{
+	
+val 	class to ArraySringFunction java.base/java.io.FileInputStream.open0 NullFunction(Native Method)
 	class to ArrayStringFunction /root/java.base/java.io.FileInputStream.open(FileInputStream.java)
 	class to ArrayStringFunction java.base/java.io.FileInputStream.<init>(FileInputStream.java)
   def to ArrayStringFunction scala.reflect.io.File.inputStream(File.scala)
@@ -19,3 +20,4 @@ def scala.reflect.io.Streamable$Chars.applyReader$(Streamable.scala)
 class scala.reflect.io.File.applyReader(File.scala)
 class scala.tools.nsc.interpreter.shell.ILoop.$anonfun$interpretAllFrom$1(ILoop.scala)
 	
+}
